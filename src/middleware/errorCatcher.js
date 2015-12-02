874387ad729b15bd0b40941be0b473b65f7761c6
gvergnaud <gabriel.vergnaud@hetic.net>
2015-12-02 13:48:21 +0100

    update errorCatcher

--- src/middleware/errorCatcher.js
+++ src/middleware/errorCatcher.js
@@ -2,7 +2,7 @@ const errorCatcherMiddleware = ({dispatch, getState}) => next => action => {
   try {
     return next(action);
   } catch(e) {
-    console.error('Action Error :', action.type, ':', e.message)
+    console.error('Action Error :', action.type, ': \n', e.stack)
   }
 }
 
